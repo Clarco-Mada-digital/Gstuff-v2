a1363cea42f88c7d99d88e0a30ce0e066eead25d
clark <clarco.dev@mada-digital.net>
2025-02-27 16:01:51 +0300

    ...

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -406,7 +406,7 @@
                       </button>
                   </div>
                   <!-- Modal body -->
-                  <div x-data="loginForm" class="p-4 md:p-5">
+                  <div x-data="loginForm" class="relative p-4 md:p-5">
 
                     {{-- Message du formulaire --}}
                     <div x-show="message" class="flex items-center p-4 mb-4 text-sm  border rounded-lg dark:bg-gray-800" :class="status ? 'text-green-800 border-green-300 bg-green-50 dark:text-green-400 dark:border-green-800' : 'text-red-800 bg-red-50 border-red-300 dark:border-red-800 dark:text-red-400' " role="alert">
@@ -440,7 +440,13 @@
                                 <a href="{{ route('password.request')}}" class="text-sm text-green-gs hover:underline hover:text-amber-300 dark:text-green-gs">{{__('Mot de passe oublié')}}</a>
                               @endif
                           </div>
-                          <button type="submit" class="w-full text-white bg-green-gs hover:bg-amber-300 hover:text-green-gs focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-gs dark:hover:bg-green-gs/30">{{__('Se connecter')}}</button>
+                          <button type="submit" class="w-full text-white bg-green-gs hover:bg-amber-300 hover:text-green-gs focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-gs dark:hover:bg-green-gs/30">
+                            <svg x-show="loadingRequest" aria-hidden="true" class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                            </svg>
+                            {{__('Se connecter')}}
+                          </button>
                           <div class="flex items-center justify-center">
                             <div class="flex-1 bg-black h-1"></div>
                             <span class="font-dm-serif p-2 text-xl">Ou</span>
@@ -547,8 +553,10 @@
               password: '',
               remember: false,
               message: '',
+              loadingRequest: false,
               status: true,
               async submitForm() {
+                this.loadingRequest = true;
                 form = document.getElementById('loginForm');
                 this.message = "";
                   try {
@@ -567,17 +575,20 @@
 
                       if (response.ok) {
                         const data = await response.json();
+                        this.loadingRequest = false;
                         this.message = data.message;
                         this.status = data.success;
                         // Redirige l'utilisateur ou effectue une autre action
                         window.location.href = '{{ route('profile') }}';
                       } else {
                         const error = await response.json();
+                        this.loadingRequest = false;
                         this.message = error.message;
                         this.status = error.success;
                       }
                   } catch (error) {
                       this.message = 'Une erreur est survenue. Veuillez réessayer.';
+                      this.loadingRequest = false;
                       this.status = false;
                   }
               }
