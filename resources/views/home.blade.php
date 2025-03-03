20a8d56f946fe32d2a081db0aac7e683001c76f1
clark <clarco.dev@mada-digital.net>
2025-03-03 16:57:06 +0300

    ...

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,6 @@
 
   @section('content')
 
-  {{-- @foreach ($apiData['users'] as $salon)
-     {{ dd($salon)}}
-  @endforeach --}}
-
     {{-- Hero content --}}
     <div class="relative flex items-center justify-center flex-col gap-8 w-full px-3 py-20 lg:h-[418px] bg-no-repeat" style="background: url('images/Hero image.jpeg') center center /cover;">
       <div class="w-full h-full z-0 absolute inset-0 to-0% right-0% bg-green-gs/65"></div>
@@ -186,24 +182,6 @@
 
       </div>
     </div>
-
-    {{-- <div class="relative py-10 w-full overflow-hidden">
-      <div class="bg-[#E4F1F1] absolute top-0 right-0 w-full h-full z-0"></div>
-      <div class="w-full flex items-center justify-center gap-5 flex-nowrap overflow-hidden">
-      @foreach ([1, 2, 3] as $item)
-      <div class=" @if ($item==2) scale-100 @else scale-75 @endif min-w-[300px] lg:w-[625px] h-[250px] p-5 bg-white rounded-lg flex flex-col items-center justify-center gap-4 text-xl lg:text-3xl z-10">
-        <span class="text-center w-[80%] mx-auto">"Amazing experience i love it a lot. Thanks to the team that dreams come true, great!"</span>
-        <div class="flex items-center w-full justify-center gap-4">
-          <img class="w-12 h-12 rounded-full" src="icons/user_icon.svg" alt="user_default icon" srcset="user icon">
-          <div class="flex flex-col font-bold">
-            <span class="font-dm-serif text-base lg:text-2xl text-green-800">Lassy Chester</span>
-            <span class="text-sm lg:text-base">Escort</span>
-          </div>
-        </div>
-      </div>
-      @endforeach
-      </div>
-    </div> --}}
     <x-FeedbackSection />
 
     <div class="bg-white py-10 w-full overflow-x-hidden flex items-center justify-center flex-col gap-10">
@@ -220,73 +198,7 @@
       </div>
     </div>
 
-    {{-- <div class="relative flex flex-col items-start justify-center w-full h-[375px]" style="background: url('images/girl_deco_image_001.jpg') center center /cover">
-      <div class="text-white flex flex-col gap-4 container mx-auto px-3">
-        <h3 class="font-dm-serif text-3xl lg:text-5xl font-bold w-full lg:w-[40%]">Inscrivez vous dès aujourd'hui sur Gstuff ...</h3>
-        <span class="font-dm-serif">La meilleure plateforme érotique en Suisse !</span>
-        <div class="z-10 w-45">
-          <a href="#" type="button" class="flex items-center justify-center gap-2 btn-gs-gradient text-black font-bold focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-4 py-2 text-center dark:focus:ring-blue-800">S'inscrire <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.006 21.308l-1.064-1.064L15.187 12L6.942 3.756l1.064-1.064L17.314 12z"/></svg>
-          </a>
-        </div>
-      </div>
-
-    </div> --}}
     <x-CallToActionInscription />
-
-    {{-- Nou contactez --}}
-    {{-- <div class="flex flex-col items-center justify-center gap-10 lg:container mx-auto py-20">
-      <div class="flex flex-col justify-center gap-5 w-full lg:w-[1140px] h-[255px] px-10 lg:px-20 text-white" style="background: url('images/girl_deco_contact.jpg') center center /cover">
-        <h2 class="text-3xl lg:text-5xl font-dm-serif font-bold">Nous contacter</h2>
-        <p>Besoin d'information ou de conseils ? Contactez-nous dès maintenat !</p>
-        <div class="z-10 mt-5">
-          <a href="#" type="button" class="w-52 flex items-center justify-center gap-2 btn-gs-gradient text-black font-bold focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg px-4 py-2 text-center text-sm lg:text-base dark:focus:ring-blue-800">Nous écrire <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.006 21.308l-1.064-1.064L15.187 12L6.942 3.756l1.064-1.064L17.314 12z"/></svg>
-          </a>
-        </div>
-      </div>
-
-      <h3 id="FAQ" class="text-3xl lg:text-5xl font-dm-serif text-green-gs">Questions fréquentes</h3>
-      <div id="accordion-collapse text-wrap w-full lg:min-w-[1114px]" data-accordion="collapse">
-        <h2 id="accordion-collapse-heading-1" class="w-full lg:min-w-[1114px]">
-          <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-            <span class="flex items-center"><svg class="w-5 h-5 me-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>Est-ce que le shooting photo est obligatoire?</span>
-            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-            </svg>
-          </button>
-        </h2>
-        <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-          <div class="p-5 border border-b-0 border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">Oui, c’est ce qui rend la plateforme si unique. Toutes les photos publiées sur GStuff ont été réalisées par notre équipe.</p>
-          </div>
-        </div>
-        <h2 id="accordion-collapse-heading-2" class="w-full lg:min-w-[1114px]">
-          <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
-            <span class="flex items-center"><svg class="w-5 h-5 me-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>Avez-vous des appartements à louer?</span>
-            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-            </svg>
-          </button>
-        </h2>
-        <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
-          <div class="p-5 border border-b-0 border-gray-200 bg-white dark:border-gray-700">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">FNous ne louons pas d’appartements pour les escorts.</p>
-          </div>
-        </div>
-        <h2 id="accordion-collapse-heading-3" class="w-full lg:min-w-[1114px]">
-          <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
-            <span class="flex items-center"><svg class="w-5 h-5 me-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>Combien puis-je gagner?</span>
-            <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-            </svg>
-          </button>
-        </h2>
-        <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
-          <div class="p-5 border border-t-0 border-gray-200 bg-white dark:border-gray-700">
-            <p class="mb-2 text-gray-500 dark:text-gray-400">Le revenu mensuel des escortes sur Gstuff dépend de nombreux critères mais il peut atteindre plusieurs dizaines de milliers de francs suisses par mois</p>
-          </div>
-        </div>
-      </div>
-    </div> --}}
 
     <x-CallToActionContact />
 
