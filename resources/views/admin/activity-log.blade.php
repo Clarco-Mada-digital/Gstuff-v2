9e252eccd501fac1036fb43ebfff51f2f05b3d6e
clark <clarco.dev@mada-digital.net>
2025-04-04 10:57:37 +0300

    '...'

--- resources/views/admin/activity-log.blade.php
+++ resources/views/admin/activity-log.blade.php
@@ -8,6 +8,7 @@
 <div class="md:ml-64 px-5">
     <h2 class="font-dm-serif text-4xl font-bold text-center w-full">Activity</h2>
   @foreach($activities as $activity)
+  <hr class="my-3 text-green-gs" />
   <div class="activity-item">
       <div class="activity-icon">
           @switch($activity->event)
@@ -39,5 +40,6 @@
       </div>
   </div>
   @endforeach
+  <hr class="my-3 text-green-gs" />
 </div>
 @endsection
