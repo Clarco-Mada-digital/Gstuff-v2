92c771128e67596b9c219100828e587eb91c582a
clark <clarco.dev@mada-digital.net>
2025-04-11 09:33:23 +0300

    ...

--- app/View/Components/footer.php
+++ app/View/Components/footer.php
@@ -28,6 +28,6 @@ class footer extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.footer');
+        return view('components.footer', ['canton' => $this->cantons]);
     }
 }
