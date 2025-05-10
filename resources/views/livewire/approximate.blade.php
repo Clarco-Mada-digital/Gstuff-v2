6c9a51ff2d99eab377609ff2c11c8b55058aa26c
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-05-10 12:39:45 +0200

    saturday edit seed

--- resources/views/livewire/approximate.blade.php
+++ resources/views/livewire/approximate.blade.php
@@ -52,7 +52,7 @@
         <div class="w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 items-center mb-4 gap-4">
             @foreach ($escorts as $escort)
             <div class="escort-card" data-distance="{{ $escort['distance'] }}">
-                <livewire:escort-card name="{{ $escort['escort']['prenom'] }}" canton="{{ $escort['canton']['nom'] }}" ville="{{ $escort['ville']['nom'] }}" avatar="{{ $escort['escort']['avatar'] }}" escortId="{{ $escort['escort']['id'] }}" distance="{{ $escort['distance'] }}" />
+                <livewire:escort-card name="{{ $escort['escort']['prenom'] ?? '' }}" canton="{{ $escort['canton']['nom'] ?? '' }}" ville="{{ $escort['ville']['nom'] ?? '' }}" avatar="{{ $escort['escort']['avatar'] ?? '' }}" escortId="{{ $escort['escort']['id'] ?? '' }}" distance="{{ $escort['distance'] ?? '' }}" />
             </div>
             @endforeach
         </div>
