7a37083bf9a3ec75d6d242c4c53268a44cd30477
clark <clarco.dev@mada-digital.net>
2025-05-16 12:39:51 +0300

    ...

--- resources/views/livewire/users-search.blade.php
+++ resources/views/livewire/users-search.blade.php
@@ -11,7 +11,7 @@
 
       <!-- Sélection des cantons -->
       <div class="w-full flex flex-col md:flex-row items-center justify-center text-sm xl:text-base gap-2 mb-3">
-          <select wire:model.debounce.500ms="selectedCanton" id="small" class="block w-1/3 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-amber-500 focus:border-amber-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500">
+          <select wire:model.live.debounce.500ms="selectedCanton" id="small" class="block w-1/3 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-amber-500 focus:border-amber-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500">
               <option value="">{{__('search_modal.cantons')}}</option>
               @foreach ($cantons as $canton)
                   <option value="{{ $canton->id }}">{{ $canton->nom }}</option>
@@ -19,7 +19,7 @@
           </select>
 
           <!-- Sélection des villes -->
-          <select wire:model.debounce.500ms="selectedVille" id="small" class="block w-1/3 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-amber-500 focus:border-amber-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500" {{ $villes->isEmpty() ? 'disabled' : '' }}>
+          <select wire:model.live.debounce.500ms="selectedVille" id="small" class="block w-1/3 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-amber-500 focus:border-amber-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500" {{ $villes->isEmpty() ? 'disabled' : '' }}>
               <option value="">{{ $villes->isEmpty() ? __('search_modal.choose_canton') : __('search_modal.cities') }}</option>
               @foreach ($villes as $ville)
                   <option value="{{ $ville->id }}">{{ $ville->nom }}</option>
@@ -27,7 +27,7 @@
           </select>
 
           <!-- Sélection du genre -->
-          <select wire:model.debounce.500ms="selectedGenre" id="small" class="block w-1/3 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-amber-500 focus:border-amber-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500">
+          <select wire:model.live.debounce.500ms="selectedGenre" id="small" class="block w-1/3 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-amber-500 focus:border-amber-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500">
               <option value="">{{__('search_modal.gender')}}</option>
               <option value="Femme">{{__('search_modal.female')}}</option>
               <option value="Homme">{{__('search_modal.male')}}</option>
@@ -43,7 +43,7 @@
       <div class="flex flex-wrap items-center justify-center gap-2 mb-3 font-bold text-sm xl:text-base">
           @foreach ($salonCategories as $categorie)
               <div wire:key="salon-{{ $categorie->id }}">
-                  <input wire:model.debounce.500ms="selectedCategories" type="checkbox" name="categories[]" id="categorie{{ $categorie->id }}" value="{{ $categorie->id }}" class="hidden peer">
+                  <input wire:model.live.debounce.500ms="selectedCategories" type="checkbox" name="categories[]" id="categorie{{ $categorie->id }}" value="{{ $categorie->id }}" class="hidden peer">
                   <label for="categorie{{ $categorie->id }}" class="p-2 text-center border border-amber-400 bg-white rounded-lg hover:bg-green-gs hover:text-amber-400 peer-checked:bg-green-gs peer-checked:text-amber-400">
                       {{ $categorie->nom }}
                   </label>
@@ -54,7 +54,7 @@
       <div class="flex flex-wrap items-center justify-center gap-2 font-bold text-sm xl:text-base">
           @foreach ($escortCategories as $categorie)
               <div wire:key="escort-{{ $categorie->id }}">
-                  <input wire:model.debounce.500ms="selectedCategories" type="checkbox" name="categories[]" id="categorie{{ $categorie->id }}" value="{{ $categorie->id }}" class="hidden peer">
+                  <input wire:model.live.debounce.500ms="selectedCategories" type="checkbox" name="categories[]" id="categorie{{ $categorie->id }}" value="{{ $categorie->id }}" class="hidden peer">
                   <label for="categorie{{ $categorie->id }}" class="p-2 text-center border border-amber-400 bg-white rounded-lg hover:bg-green-gs hover:text-amber-400 peer-checked:bg-green-gs peer-checked:text-amber-400">
                       {{ $categorie->nom }}
                   </label>
