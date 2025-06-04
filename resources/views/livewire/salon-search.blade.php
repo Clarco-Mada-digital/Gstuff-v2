a375694ae476ea649b5e00dc898a2c91743a99cb
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-06-04 09:38:06 +0200

    confiramtion age

--- resources/views/livewire/salon-search.blade.php
+++ resources/views/livewire/salon-search.blade.php
@@ -188,13 +188,33 @@
        </div>
 
 
+     
+
         <button wire:click="resetFilter"
-            class="font-dm-serif hover:bg-green-gs group my-2 flex items-center gap-2 rounded-lg border border-gray-400 bg-white p-2 text-gray-600 hover:text-white">
-            {{ __('salon-search.reset_filters') }}
-            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                <path fill="currentColor"
-                    d="M22.448 21A10.86 10.86 0 0 0 25 14A10.99 10.99 0 0 0 6 6.466V2H4v8h8V8H7.332a8.977 8.977 0 1 1-2.1 8h-2.04A11.01 11.01 0 0 0 14 25a10.86 10.86 0 0 0 7-2.552L28.586 30L30 28.586Z" />
-            </svg>
+            class="font-dm-serif hover:bg-green-gs group my-2 flex items-center gap-2 rounded-lg border border-gray-400 bg-white p-2 text-gray-600 hover:text-white"
+            wire:loading.attr="disabled">
+            <span wire:loading.remove wire:target="resetFilter">
+                {{ __('escort-search.reset_filters') }}
+            </span>
+            <span wire:loading wire:target="resetFilter" class="flex items-center">
+                {{ __('escort-search.resetting') }}
+            </span>
+            <span wire:loading.remove wire:target="resetFilter">
+                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                    <path fill="currentColor"
+                        d="M22.448 21A10.86 10.86 0 0 0 25 14A10.99 10.99 0 0 0 6 6.466V2H4v8h8V8H7.332a8.977 8.977 0 1 1-2.1 8h-2.04A11.01 11.01 0 0 0 14 25a10.86 10.86 0 0 0 7-2.552L28.586 30L30 28.586Z" />
+                </svg>
+            </span>
+            <span wire:loading wire:target="resetFilter" class="ml-2">
+                <svg class="h-5 w-5 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                        stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                    </path>
+                </svg>
+            </span>
         </button>
 
     </div>
