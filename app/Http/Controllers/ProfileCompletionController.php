4d338e30696c04d8e2751a00bce0690dbd4bb670
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-08-05 08:39:37 +0200

    debug

--- app/Http/Controllers/ProfileCompletionController.php
+++ app/Http/Controllers/ProfileCompletionController.php
@@ -244,6 +244,8 @@ class ProfileCompletionController extends Controller
             
                     return $invitation;
                 });
+
+                // dd($salonAssociers);
             
 
 
