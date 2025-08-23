cffdd003d383be0e9b8cdf7247fa4566e856cfd7
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-08-23 11:15:24 +0200

    ok

--- resources/views/components/associated-swiper-card.blade.php
+++ resources/views/components/associated-swiper-card.blade.php
@@ -121,7 +121,8 @@
             breakpoints: {
                 320: { slidesPerView: 1 },
                 640: { slidesPerView: 2 },
-                1024: { slidesPerView: 2 },
+                1024: { slidesPerView: 3 },
+                1280: { slidesPerView: 2 },
             },
         });
     });
