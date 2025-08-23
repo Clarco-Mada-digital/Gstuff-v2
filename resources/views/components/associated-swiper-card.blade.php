45ca17a15fb042bfc3fb33c00222496e2ecd4ae1
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-08-23 10:46:19 +0200

    ok

--- resources/views/components/associated-swiper-card.blade.php
+++ resources/views/components/associated-swiper-card.blade.php
@@ -8,7 +8,7 @@
 @if ($data->isNotEmpty())
 
    
-    <div class="flex min-w-full flex-col items-center justify-center gap-4 xl:w-1/2">
+<div class="flex items-center justify-between gap-5 py-5">
                                 <h3 class="font-roboto-slab text-green-gs text-xl">{{ __('profile.favorite_salons') }}</h3>
                               
 
