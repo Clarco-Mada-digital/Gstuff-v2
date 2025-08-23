8356a044c502b025f300b706abcc2c3a5f831e44
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-08-23 11:23:01 +0200

    ok

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1363,7 +1363,7 @@
                         <div class="flex items-center justify-center py-5 md:justify-start">
                             <h2 class="text-2xl font-bold font-roboto-slab text-green-gs">{{ __('profile.my_favorites') }}</h2>
                         </div>
-                        <div class="grid w-full grid-cols-1 gap-10 xl:grid-cols-2 xl:gap-10 bg-red-500 ">
+                        <div class="grid w-full grid-cols-1 gap-10 xl:grid-cols-2 xl:gap-10 ">
                             <x-associated-swiper-card :data="$escortFavorites" type="escort" profil="user"/>
                             <x-associated-swiper-card :data="$salonFavorites" type="salon" profil="user"/>
                         </div>
