9d223b7b30bb6b8c0987b9a414c1063a668bd016
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-09-19 09:17:22 +0200

    ok

--- resources/views/components/langue-select-escort.blade.php
+++ resources/views/components/langue-select-escort.blade.php
@@ -39,7 +39,7 @@
                 <svg id="toggle-icon-langue" class="w-3 h-3 text-supaGirlRose transition-transform" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                     <path id="toggle-icon-path-langue" stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
                 </svg>
-                <span id="toggle-label-langue">{{ __('escort-search.showMore') }}</span>
+                <span id="toggle-label-langue" class="text-supaGirlRose">{{ __('escort-search.showMore') }}</span>
             </button>
         </div>
     @endif
