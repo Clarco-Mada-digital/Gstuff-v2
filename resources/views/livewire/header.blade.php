0d45db1d5f6213d6709cc713f508e7e4fcbe2dad
GitHubFabrino <rakotoharilalainafabrino@gmail.com>
2025-10-02 09:17:42 +0200

    login

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -143,9 +143,28 @@
             @guest
                 {{-- Btn de connexion --}}
                 <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button"
-                    class="btn-complementaryColorViolet bg-complementaryColorViolet hidden cursor-pointer rounded-lg px-4 py-2 text-center text-sm font-bold text-white focus:outline-none lg:order-1 xl:block">
-                    {{ __('header.login_register') }}
-                </button>
+    class="bg-complementaryColorViolet focus:ring-supaGirlRose/50 group relative flex transform cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-lg px-4 py-2 xl:px-5 xl:py-2.5 text-center text-sm font-bold text-white transition-all duration-300 hover:scale-[1.02] hover:bg-opacity-90 hover:shadow-lg focus:outline-none focus:ring-4 lg:order-1 xl:block">
+    
+    <span class="relative z-10 flex items-center gap-2 whitespace-nowrap">
+        <span class="relative text-xs xl:text-sm">
+            {{ __('header.login_register') }}
+            <span class="absolute -bottom-0.5 left-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
+        </span>
+        <svg class="h-4 w-4 transition-transform duration-300 group-hover:translate-x-1"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
+            <path fill="currentColor" d="m8.006 21.308l-1.064-1.064L15.187 12L6.942 3.756l1.064-1.064L17.314 12z" />
+        </svg>
+    </span>
+
+    <span class="pointer-events-none absolute inset-0 flex items-center justify-center">
+        <span class="absolute h-0 w-0 rounded-full bg-white opacity-0 transition-all duration-1000 group-hover:h-32 group-hover:w-full group-hover:opacity-10"></span>
+    </span>
+
+    <span class="absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        <span class="absolute inset-0 rounded-lg border-2 border-white/80 transition-all duration-300"></span>
+    </span>
+</button>
+
                 <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button"
                     class="text-complementaryColorViolet hover:bg-complementaryColorViolet inline-flex cursor-pointer items-center rounded-full p-2 text-center text-sm font-medium focus:outline-none xl:order-1 xl:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
